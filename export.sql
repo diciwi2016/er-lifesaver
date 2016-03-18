29ad23106afc8158b06c1ea9f6e1b903bbbcd9f5
Kathy Lau <imobilep501@gmail.com>
2016-03-17 21:53:26 -0400

    sql

---
+++ export.sql
@@ -0,0 +1,11 @@
+--
+-- MySQL 5.6.17
+-- Fri, 18 Mar 2016 01:47:53 +0000
+--
+
+CREATE TABLE `waittimes` (
+   `hospitalname` text,
+   `wait` int(11)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+-- [Table `waittimes` is empty]
